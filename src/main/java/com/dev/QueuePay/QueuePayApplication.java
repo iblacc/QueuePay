6bcb46a8efb1cd6a6c84fda89ffbfcb0775a6daa
Meralds-World <grace.chidumaga@gmail.com>
2020-03-03 09:35:43 +0100

    first commit

---
+++ src/main/java/com/dev/QueuePay/QueuePayApplication.java
@@ -0,0 +1,13 @@
+package com.dev.QueuePay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/QueuePay/QueuePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.QueuePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
