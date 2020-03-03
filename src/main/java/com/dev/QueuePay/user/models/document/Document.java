abde66028d70852347c6b33a7e1f8c51ee328dbd
Meralds-World <grace.chidumaga@gmail.com>
2020-03-03 11:09:22 +0100

    create user model

---
+++ src/main/java/com/dev/QueuePay/user/models/document/Document.java
@@ -0,0 +1,31 @@
+package com.dev.QueuePay.user.models.document;
+
+import com.dev.QueuePay.user.models.user.User;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+public class Document {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String documentName;
+
+    private String documentType;
+
+    @Lob
+    private byte[] data;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Document(String documentName, String documentType, byte[] data) {
+       this.documentName = documentName;
+       this.documentType = documentType;
+       this.data = data;
+    }
+}
