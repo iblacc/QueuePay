41b3e2c1d65883393d19ffe07bc0fd0ccae294ba
Meralds-World <grace.chidumaga@gmail.com>
2020-03-03 13:47:44 +0100

    make changes to user data transfer objects

--- src/main/java/com/dev/QueuePay/user/dto/InfoRequest.java
+++ src/main/java/com/dev/QueuePay/user/dto/InfoRequest.java
@@ -1,4 +1,66 @@
 package com.dev.QueuePay.user.dto;
 
+import com.dev.QueuePay.user.models.document.Document;
+
+import javax.persistence.Lob;
+import javax.validation.constraints.NotBlank;
+
 public class InfoRequest {
+    @NotBlank
+    private String description;
+
+    @NotBlank
+    @Lob
+    private byte[] logo;
+
+    @NotBlank
+    private String documentName;
+
+    @NotBlank
+    private String documentType;
+
+    @NotBlank
+    @Lob
+    private byte[] document;
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getLogo() {
+        return logo;
+    }
+
+    public void setLogo(byte[] logo) {
+        this.logo = logo;
+    }
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    public byte[] getDocument() {
+        return document;
+    }
+
+    public void setDocument(byte[] document) {
+        this.document = document;
+    }
 }
--- src/main/java/com/dev/QueuePay/user/dto/MerchantSummary.java
+++ src/main/java/com/dev/QueuePay/user/dto/MerchantSummary.java
@@ -1,4 +1,27 @@
 package com.dev.QueuePay.user.dto;
 
 public class MerchantSummary {
+    private String businessName;
+    private String email;
+
+    public MerchantSummary(String businessName, String email) {
+        this.businessName = businessName;
+        this.email = email;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
